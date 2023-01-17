ba3e79030b89ac0356bfca4d5962ecc179c2d274
saintskeeper <walterdayjr@gmail.com>
2023-01-16 21:31:07 -0500

    adding updates from pre-commit

--- iac/terraform-infra/modules/gke-quickstart/gke.tf
+++ iac/terraform-infra/modules/gke-quickstart/gke.tf
@@ -56,8 +56,8 @@ resource "google_container_node_pool" "primary_nodes" {
 
 
 # # Kubernetes provider
-# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
-# # It references the variables and resources provisioned in this file. 
+# # The Terraform Kubernetes Provider configuration below is used as a learning reference only.
+# # It references the variables and resources provisioned in this file.
 # # We recommend you put this in another file -- so you can have a more modular configuration.
 # # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
 # # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
@@ -73,4 +73,3 @@ resource "google_container_node_pool" "primary_nodes" {
 #   client_key             = google_container_cluster.primary.master_auth.0.client_key
 #   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
 # }
-
