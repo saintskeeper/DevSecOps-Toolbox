279a225ac5e81376ea8535a0be7b325b3228ad52
saintskeeper <walterdayjr@gmail.com>
2023-10-22 12:47:35 -0400

    adding the helm provider to fix eks login issues

--- iac/terraform-infra/aws-eks/dev/main.tf
+++ iac/terraform-infra/aws-eks/dev/main.tf
@@ -12,7 +12,19 @@ provider "kubernetes" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
 
 provider "kubectl" {
   apply_retry_count      = 5
@@ -210,6 +222,8 @@ module "ingres_nginx_external" {
     ]
   }
 }
+
+
 
 resource "aws_security_group" "ingress_nginx_internal" {
   name        = "ingress-nginx-internal"
